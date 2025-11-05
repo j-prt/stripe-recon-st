0cef749ddb7cf53b3518a6d6ab85fddc8e0a66e3
j-prt <peart.l.joe@gmail.com>
2025-11-05 14:41:40 -0800

    add url gen

---
+++ daterange.py
@@ -0,0 +1,38 @@
+"""Reconcile Stripe deposits with Commerce7 orders.
+
+Input args:
+    - [1]  Path to Stripe csv
+
+Output: Niche C7 URL to use to find the correct date range of orders."""
+
+import csv
+import sys
+from datetime import datetime, timedelta
+
+DELTA_MIN = 5
+NICHE_URL = 'https://niche-wine-company.admin.platform.commerce7.com/store/order?orderPaidDate=btw:'
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        reader = csv.reader(f)
+
+        # Skip header
+        next(reader)
+
+        # Get the first date
+        start = datetime.strptime(next(reader)[2], '%Y-%m-%d %H:%M')
+
+        for row in reader:
+            # Timestamp at index 2 of the Stripe file
+            last = row
+
+        finish = datetime.strptime(last[2], '%Y-%m-%d %H:%M')
+
+        delta = timedelta(minutes=DELTA_MIN)
+
+        # Format used in C7: 2025-06-24T00:00:00.000Z
+        start = datetime.strftime(start - delta, '%Y-%m-%dT%H:%M:%S.000Z')
+        finish = datetime.strftime(finish + delta, '%Y-%m-%dT%H:%M:%S.000Z')
+
+        print(f'{NICHE_URL}{start}|{finish}')
